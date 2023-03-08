fa3bc3af747ce90b6b6e731f77a00e267ed1047b
Saigupta7887 <83281595+Saigupta7887@users.noreply.github.com>
2023-03-07 20:19:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+fetch('https://upadhayay.github.io/db.json')
+  .then(response => response.json())
+  .then(data => {
+    // Get the array of paper titles
+    const paperTitles = data.books.map(paper => paper.title);
+    
+    // Display the second and third titles in the HTML page
+
+    const firstTitleDiv = document.getElementById('first-title');
+    firstTitleDiv.innerText = paperTitles[0];
+
+    const secondTitleDiv = document.getElementById('second-title');
+    secondTitleDiv.innerText = paperTitles[1];
+    
+    const thirdTitleDiv = document.getElementById('third-title');
+    thirdTitleDiv.innerText = paperTitles[2];
+
+    const secTitleDiv = document.getElementById('three-title');
+    secTitleDiv.innerText = paperTitles[3];
+
+    const secoTitleDiv = document.getElementById('four-title');
+    secoTitleDiv.innerText = paperTitles[4];
+
+    const seTitleDiv = document.getElementById('six-title');
+    seTitleDiv.innerText = paperTitles[3];
+
+
+    const titleList = document.getElementById('paper-titles');
+    const ul = document.createElement('ul');
+    titleList.appendChild(ul);
+
+    paperTitles.forEach(title => {
+      const li = document.createElement('li');
+      li.textContent = title;
+      ul.appendChild(li);
+    });
+
+
+
+  });
+
+
+  
